a04ec76879c3813561919577bfd010f77b2bb698
Garnett Grant <net_grant@hotmail.com>
2022-10-24 09:46:06 -0400

    Web Dev MidTerm BoilerPlate

--- COMP229-F2020-MidTerm-Start/server/config/db.js
+++ COMP229-F2020-MidTerm-Start/server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  // "URI": "mongodb://localhost/books229",
+  "URI": "mongodb+srv://admin:D7rrgLmCC1ab8SHr@clutsermidterm.1mlereo.mongodb.net/?retryWrites=true&w=majority"
 };
